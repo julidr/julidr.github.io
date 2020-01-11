540f00139fa2c420170c8e03c35addb3961fdfeb
Juliana Diaz <juli.milena@hotmail.com>
2020-01-11 16:57:13 -0500

    [Refactor] Change project structure and update versions

--- js/carousel.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-        $('.carousel.carousel-slider').carousel({full_width: true, indicators: false, height: 600});
-});
--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-var navBar= $("#navBar");
-
-$(window).scroll(function(){
-	if($(this).scrollTop()>580){
-		navBar.addClass("navBar-scrolled");
-	}else{
-		navBar.removeClass("navBar-scrolled");
-	}
-});
